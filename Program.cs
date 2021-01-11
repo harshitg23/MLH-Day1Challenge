47385891a4fccd98e12fbe7bf1f7aa20f12cc14d
harshitg23 <69359941+harshitg23@users.noreply.github.com>
2021-01-11 11:43:09 +0530

    HelloWorld
    Day1 Challenge

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello MLH");
+            Console.WriteLine("Hello LHD");
+            Console.WriteLine("Day 1 Challenge");
+        }
+    }
+}
